7b78994e8ddce8e1794e3f3106329865dd1e51b5
Noizirom <45117806+Noizirom@users.noreply.github.com>
2019-08-14 22:16:44 -0400

    Add files via upload

---
+++ co_to_js.py
@@ -0,0 +1,34 @@
+import bpy, numpy as np, os, json as js
+
+
+desktop = os.path.expanduser("~/Desktop")
+ob = bpy.context.object
+fname = ob.name
+
+
+def get_co():
+    mode = bpy.context.active_object.mode
+    vt = bpy.context.object.data.vertices
+    bpy.ops.object.mode_set(mode='OBJECT')
+    #vertices
+    countv = len(vt)
+    co = np.empty(countv * 3, dtype=np.float32)
+    vt.foreach_get('co', co)
+    co.shape = (countv, 3)
+    return co
+
+
+def write_js(fileName, path, data):
+    cwd = os.getcwd()
+    os.chdir(path)
+    fpath = fileName + '.json'
+    with open(fpath, 'w') as f:
+        js.dump(data, f)
+    os.chdir(cwd)    
+        
+gc = get_co()
+gc = gc.tolist()
+
+
+write_js(fname, desktop, gc)
+        
